37df18717cd7c6b5e87ae65b84ed8b11acd8039c
Rob <robcodehub@gmail.com>
2023-10-23 21:28:57 +1100

    Initial Commit

---
+++ littlelemon/littlelemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  littlelemonApp.swift
+//  littlelemon
+//
+//  Created by Rob Wise on 23/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct littlelemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
